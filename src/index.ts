5918e6cd8f84a274c08c7afdb4fbc05c66616220
airtonlopsan <lopsan@msn.com>
2020-09-16 14:48:11 -0300

    Configurações iniciais feitas

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('oi')
+import express from "express";
+import { AddressInfo } from "net";
+import dotenv from 'dotenv';
+import { signup } from "./endpoints/signup";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+    } else {
+    console.error(`Failure upon starting server.`);
+    }
+    });
+
+    app.post('/user/signup', signup);
